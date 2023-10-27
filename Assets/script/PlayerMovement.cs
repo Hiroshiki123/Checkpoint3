43e8348c661f521f6f39b56e62d76188310837c4
Hiroshiki123 <79053749+Hiroshiki123@users.noreply.github.com>
2023-10-27 11:10:19 -0300

    aaaa

--- Assets/script/PlayerMovement.cs
+++ Assets/script/PlayerMovement.cs
@@ -6,23 +6,31 @@ public class PlayerMovement : MonoBehaviour
 {
     public float direction;
     public float speed;
+    public float gravity;
     public float jumptime;
     public float jumpForce;
+    public float sensorradius;
     public float variationjump;
     public float curentJumpTime;
     public float jumpForceVariation;
     private Rigidbody2D rb;
     private SpriteRenderer sr;
+    public Transform sensor;
+    private Animator ani;
+    private bool isGround;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();  
         sr = GetComponent<SpriteRenderer>();
+        rb.gravityScale = gravity;
+        ani = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        isGround = Physics2D.OverlapCircle(sensor.position, sensorradius, 1<<LayerMask.NameToLayer("Ground"));
 
         direction = Input.GetAxisRaw("Horizontal")*speed;
         if (direction < 0 )
@@ -33,7 +41,7 @@ public class PlayerMovement : MonoBehaviour
         {
             sr.flipX = false;
         }
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGround)
         {
             curentJumpTime = jumptime;
             jumpForceVariation = 2;
@@ -41,7 +49,7 @@ public class PlayerMovement : MonoBehaviour
         else if (Input.GetButton("Jump"))
         {
             curentJumpTime -= Time.deltaTime;
-            jumpForceVariation -= Time.deltaTime;
+            jumpForceVariation -= Time.deltaTime*1.5f;
         }
         else if (Input.GetButtonUp("Jump"))
         {
